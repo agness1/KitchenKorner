fa4b58f8aa1687503851193fc6a26d3e4081e7cf
Agnieszka <agni.banaszek@gmail.com>
2024-05-13 14:48:36 +0200

    Create endpoint for sending in Controller

--- src/Controller/RecipesController.php
+++ src/Controller/RecipesController.php
@@ -5,8 +5,12 @@ namespace App\Controller;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Recipe;
+use App\Form\RecipeFormType;
 use App\Repository\RecipeRepository;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class RecipesController extends AbstractController
 {
@@ -28,6 +32,38 @@ class RecipesController extends AbstractController
         return $this->render('home/home.html.twig', [
             'recipes' => $recipes
         ]);
+    }
+
+    #[Route('/recipe/create', name: 'create_recipe')]
+    public function create(Request $request): Response 
+    {
+        $recipe = new Recipe();
+        $form = $this->createForm(RecipeFormType::class, $recipe);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+          $newRecipe = $form->getData();
+          $imagePath = $form->get('imagePath')->getData();
+          if ($imagePath) {
+            $newFileName = uniqid() . '.' . $imagePath->guessExtension();
+            try {
+                $imagePath->move(
+                $this->getParameter('kernel.project_dir') . '/public/uploads', $newFileName
+                );
+            } catch (FileException $e) {
+            return new Response($e->getMessage());
+            }
+            $newRecipe->setImagePath('/uploads/' . $newFileName);
+          }
+
+          $this->em->persist($newRecipe);
+          $this->em->flush();
+
+          return $this->redirectToRoute('app_home');
+        }
+        return $this->render('recipes/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+
     }
 
     #[Route('/recipes/{id}', name: 'show_recipe', methods:['GET'])]
--- src/Form/RecipeFormType.php
+++ src/Form/RecipeFormType.php
@@ -9,23 +9,29 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class RecipeFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('instruction')
-            ->add('ingredients')
-            ->add('imagePath')
+            ->add('name', TextType::class)
+            ->add('instruction', TextAreaType::class)
+            ->add('ingredients', TextAreaType::class)
+            ->add('imagePath', FileType::class, array(
+                'required' => false,
+                'mapped' => false
+            ))
             /*->add('user', EntityType::class, [
                 'class' => User::class,
                 'choice_label' => 'id',
             ])*/
             ->add('category', EntityType::class, [
                 'class' => Category::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name',
             ])
         ;
     }
